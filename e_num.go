5b10864381019a13f06084c370a343b890547410
Jonathan Díaz <jonathan.dguevara@alumnos.udg.mx>
2020-10-01 18:20:42 -0500

    Numero e v2

--- e_num.go
+++ e_num.go
@@ -1,11 +1,16 @@
 package main
 import "fmt"
 func main(){
+	var factVal uint64 = 1
+	var x int 
 	var e float64 = 1
-	var aux float64 = 1
-	for i:=1; i<10; i++{
-		aux = aux/float64(i)
-		e = e + aux
-		fmt.Println(e)
-	}
+	fmt.Scan(&x)
+	for i:=1; i<=x; i++{
+		factVal = 1
+		for j:=i; j>=1; j-- {
+			factVal *= uint64(j)
+		}
+		e += 1/float64(factVal)
+	}	
+	fmt.Println(e)
 }
